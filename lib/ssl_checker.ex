591e69f0e10637c0b9b9f0db4243cf3982b8bc5f
Samuel Ralak <samuelralak@hotmail.com>
2018-04-16 15:14:02 +0300

    executing openssl command using System.cmd

--- lib/ssl_checker.ex
+++ lib/ssl_checker.ex
@@ -15,4 +15,14 @@ defmodule SslChecker do
   def hello do
     :world
   end
+
+  def check(host, port \\ 443) do
+	command = command(host, port)
+  	{output, 0} = System.cmd("/bin/sh", ["-c", command])
+	output
+  end
+
+  defp command(host, port) do
+  	"echo | openssl s_client -showcerts -connect #{host}:#{port} 2>/dev/null | openssl x509 -inform pem -noout -issuer -subject -dates"
+  end
 end
