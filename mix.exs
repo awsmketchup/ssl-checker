989de2081899815675830fdfc100e3841ab1c9f9
Samuel Ralak <samuelralak@hotmail.com>
2018-04-19 16:49:07 +0300

    added ex_doc

--- mix.exs
+++ mix.exs
@@ -5,8 +5,18 @@ defmodule SslChecker.MixProject do
     [
       app: :ssl_checker,
       version: "0.1.0",
+	  description: "Check host's SSL validity",
+	  source_url: "https://github.com/awsmketchup/ssl-checker",
+	  homepage_url: "https://github.com/awsmketchup/ssl-checker",
       elixir: "~> 1.6",
+	  maintainers: ["Samuel Ralak"],
       start_permanent: Mix.env() == :prod,
+	  package: [
+		  maintainers: ["Samuel Ralak"],
+		  licenses: ["MIT"],
+		  links: %{github: "https://github.com/awsmketchup/ssl-checker"},
+		  files: ~w(lib) ++ ~w(mix.exs README.md)
+	  ],
       deps: deps()
     ]
   end
@@ -21,8 +31,7 @@ defmodule SslChecker.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 end
