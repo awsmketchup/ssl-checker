ce9bf0a361665d1fec23c48b585e2f0f226db2b8
Samuel Ralak <samuelralak@hotmail.com>
2018-04-19 13:48:22 +0300

    returning map from output

--- lib/ssl_checker.ex
+++ lib/ssl_checker.ex
@@ -18,9 +18,38 @@ defmodule SslChecker do
 
   def check(host, port \\ 443) do
 	command = command(host, port)
-  	{output, 0} = System.cmd("/bin/sh", ["-c", command])
-	output
+	case System.cmd("/bin/sh", ["-c", command]) do
+		{output, 0} ->
+			output
+			|> format_output
+			|> output_to_map(%{})
+	end
   end
+
+  defp format_output(output) do
+	output
+	|> String.split("\n", trim: true)
+  end
+
+  defp output_to_map([], map), do: map
+  defp output_to_map([out | output], map) do
+	arr = out
+		|> String.replace("=", "->", global: false)
+		|> String.split("->", trim: true)
+
+	output_to_map(output,
+		map
+		|> Map.merge(get_key_val(arr))
+	)
+  end
+
+  defp get_key_val([key, val]) when key == "subject" or key == "issuer" do
+	case Regex.run(~r/O=(.*?)\//, val) do
+		nil -> %{ key => "" }
+		arr ->  %{ key => Enum.at(arr, 1) }
+	end
+  end
+  defp get_key_val([key, val]), do: %{ key => val }
 
   defp command(host, port) do
   	"echo | openssl s_client -showcerts -connect #{host}:#{port} 2>/dev/null | openssl x509 -inform pem -noout -issuer -subject -dates"
